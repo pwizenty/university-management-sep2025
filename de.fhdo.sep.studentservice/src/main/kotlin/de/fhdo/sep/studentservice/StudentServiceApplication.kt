08e2277e50e3627bfcaeddf8f52d850d9b4f40d6
Philip Wizenty <philip.wizenty@fh-dortmund.de>
2025-04-02 16:58:50 +0200

    StudentService: Enable discovery service connection

--- de.fhdo.sep.studentservice/src/main/kotlin/de/fhdo/sep/studentservice/StudentServiceApplication.kt
+++ de.fhdo.sep.studentservice/src/main/kotlin/de/fhdo/sep/studentservice/StudentServiceApplication.kt
@@ -2,8 +2,10 @@ package de.fhdo.sep.studentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class StudentServiceApplication
 
 fun main(args: Array<String>) {
